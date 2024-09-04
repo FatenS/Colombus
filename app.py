e8bbb3e88e716736c77b76b7e065b8d497a30ab5
chaker-sidhom <85159247+chaker-sidhom@users.noreply.github.com>
2024-09-04 09:42:42 +0200

    dockerize python app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from scheduler import scheduler, start_scheduler
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pass123@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pass123@db:5432/postgres'
 db.init_app(app)
 app.register_blueprint(admin_bp, url_prefix='/admin')
 app.register_blueprint(user_bp, url_prefix='/')
@@ -20,5 +20,5 @@ socketio = init_socketio(app)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5001)
     socketio.run(app, debug=True)
